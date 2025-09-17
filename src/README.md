21bde116b743d2a7d29419baccb96d9b37101e58
Sathurshan Arulmohan <arulmohs@mcmaster.ca>
2025-09-17 11:19:02 -0400

    [#47] CMakeLists.txt defined

--- src/README.md
+++ src/README.md
@@ -1,4 +1,4 @@
-# Project Name Source Code
+# Audio360 Source Code
 
 The folders and files for this project are as follows:
 
---
+++ test/test_template.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+/** @brief The test is a sample test. It asserts true and false. */
+TEST(SampleTest, TestTemplate) {
+  EXPECT_TRUE(true);
+  EXPECT_FALSE(false);
+}
