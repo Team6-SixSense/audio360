21fa18cd31201051dc0e932e9671f224fb1e4812
Omar Alam <alamo2@mcmaster.ca>
2025-11-19 10:57:04 -0500

    [#402] Cleaned up the main function so its not spaghetti anymore

---
+++ src/runtimes/runtime_fft.h
@@ -0,0 +1,20 @@
+#ifndef RUNTIME_FFT_H
+#define RUNTIME_FFT_H
+
+#include "features/signal_processing/fft.h"
+
+void main_runtime_fft()
+{
+
+    std::vector<double> test_vec;
+    FFT fft_test(static_cast<uint16_t>(test_vec.size()));
+
+    while(1)
+    {
+
+    }
+
+}
+
+
+#endif
