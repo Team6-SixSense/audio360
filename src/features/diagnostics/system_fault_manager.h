32fd18d3d7ca9743ca9fd3fdb24dab0bc6a53745
Sathurshan Arulmohan <arulmohs@mcmaster.ca>
2026-01-08 16:52:18 -0500

    [#439] System fault manager module

---
+++ src/features/diagnostics/system_fault_manager.h
@@ -0,0 +1,51 @@
+/**
+ ******************************************************************************
+ * @file    system_fault_manager.h
+ * @brief   System Fault Manager header.
+ ******************************************************************************
+ */
+
+#pragma once
+
+#include <queue>
+#include <set>
+#include <string>
+
+#include "peripheral_error.h"
+
+/** @brief System fault state. */
+enum SystemFaultState {
+  NO_FAULT,
+  SETUP_FAULT,
+  CLASSIFICATION_FAULT,
+  DIRECTIONAL_ANALYSIS_FAULT,
+  VISUALIZATION_FAULT,
+};
+
+/** @brief Module to monitors system health and manage critical system faults.
+ */
+class SystemFaultManager {
+ public:
+  /** @brief Construct a new System Fault Manager object. */
+  SystemFaultManager();
+
+  /**
+   * @brief Handles faults that occurred during peripheral set-up.
+   *
+   * @param errors Set of peripheral set-up errors.
+   */
+  void handlePeripheralSetupFaults(std::set<PeripheralError>* errors);
+
+ private:
+  /**
+   * @brief Enter unrecoverable state. Only call this when the system has
+   * no way of fixing itself. This is meant to control the system behaviour when
+   * critical faults occur that prevent from performing core functionalities.
+   *
+   * @param error Error that has caused to enter the unrecoverable state.
+   */
+  void enterUnrecoverableState(std::string error);
+
+  /** @brief Current system fault state. */
+  SystemFaultState state{NO_FAULT};
+};
