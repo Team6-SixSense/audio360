d4ce07897363b10244c67d3ca00cfa636b5b001d
Sathurshan Arulmohan <arulmohs@mcmaster.ca>
2026-01-15 12:41:56 -0500

    [#467] Mic relative distance updated

--- src/helper/constants.h
+++ src/helper/constants.h
@@ -22,10 +22,17 @@ constexpr inline float PI_32 = 3.14159265358979f;
 constexpr inline float TWO_PI_32 = 2.0 * PI_32;
 
 // Hardware constants.
+#ifndef BUILD_TEST
+constexpr inline float MIC1_2_DISTANCE_m = 0.15f;
+constexpr inline float MIC2_3_DISTANCE_m = 0.13f;
+constexpr inline float MIC3_4_DISTANCE_m = 0.15f;
+constexpr inline float MIC4_1_DISTANCE_m = 0.13f;
+#else
 constexpr inline float MIC1_2_DISTANCE_m = 0.10f;
 constexpr inline float MIC2_3_DISTANCE_m = 0.10f;
 constexpr inline float MIC3_4_DISTANCE_m = 0.10f;
 constexpr inline float MIC4_1_DISTANCE_m = 0.10f;
+#endif
 
 // Physics constants.
 constexpr inline float SOUND_AIR_mps = 343.0f;
