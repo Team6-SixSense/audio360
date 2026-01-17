c186dc09f9d8cc4931c347a8fa89c1f6e9f200d7
Sathurshan Arulmohan <arulmohs@mcmaster.ca>
2026-01-17 16:00:43 -0500

    [#459] android communication protocol

---
+++ src/features/classification/classificationLabel.h
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @file    classificationLabel.h
+ * @brief   Classification label header code
+ ******************************************************************************
+ */
+
+#pragma once
+
+/** @brief Enum holding all classification labels. */
+enum class ClassificationLabel {
+  Unknown = 0,
+  Siren = 1,
+  Jackhammer = 2,
+  CarHorn = 3,
+};
