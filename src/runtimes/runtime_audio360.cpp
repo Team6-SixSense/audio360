c8dc36032fc5549c9fa4140bce2907c88dd738a0
Sathurshan Arulmohan <arulmohs@mcmaster.ca>
2026-02-28 22:17:59 -0500

    [#549] Fault handler for DOA & System Fault Manager state machine

--- src/runtimes/runtime_audio360.cpp
+++ src/runtimes/runtime_audio360.cpp
@@ -11,6 +11,7 @@
 #include "classification.h"
 #include "doa.h"
 #include "embedded_mic.h"
+#include "exceptions.hpp"
 #include "filter.hpp"
 #include "logging.hpp"
 #include "packet.h"
@@ -203,8 +204,14 @@ float runDoA(bool newData) {
     mic4Data[i] = static_cast<float>(micB1Buffer[start + i]);
   }
 
-  float angle = doa.calculateDirection(mic1Data, mic2Data, mic3Data, mic4Data,
-                                       DOA_Algorithms::GCC_PHAT);
+  float angle{0.0};
+  try {
+    angle = doa.calculateDirection(mic1Data, mic2Data, mic3Data, mic4Data,
+                                   DOA_Algorithms::GCC_PHAT);
+    systemFaultManager.clearDoaError();
+  } catch (const AudioProcessingException& e) {
+    systemFaultManager.reportDoaError();
+  }
 
   return angle;
 }
@@ -227,7 +234,14 @@ std::string runClassification(bool newData) {
     mic1Data[i] = static_cast<float>(micA1Buffer[start + i]);
   }
 
-  classifier.Classify(mic1Data);
+  std::string classification{};
+  try {
+    classifier.Classify(mic1Data);
+    classification = classifier.getClassificationLabel();
+    systemFaultManager.clearClassficationError();
+  } catch (const std::exception& e) {
+    systemFaultManager.reportClassificationError();
+  }
 
-  return classifier.getClassificationLabel();
+  return classification;
 }
